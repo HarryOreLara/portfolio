83b274f3744a9627ff4a7279098493fb7ac3c0c2
HarryOreLara <harryorelara@gmail.com>
2024-05-15 23:13:35 -0500

    Generando la estructura principal

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./components/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./components/about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./components/contact/contact.module').then(
+        (m) => m.ContactModule
+      ),
+  },
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./components/projects/projects.module').then(
+        (m) => m.ProjectsModule
+      ),
+  },
+  {
+    path: 'notfound',
+    loadChildren: () =>
+      import('./components/notfound/notfound.module').then(
+        (m) => m.NotfoundModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'notfound',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { StyleClassModule } from 'primeng/styleclass';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule,
+    StyleClassModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
