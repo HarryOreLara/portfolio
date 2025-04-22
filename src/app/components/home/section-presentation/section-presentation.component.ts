d832c8e42d3801c801b0b00db41049bd3073a763
Harry Ore Lara <harryorelara@gmail.com>
2025-04-22 14:43:30 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/components/home/section-presentation/section-presentation.component.ts
+++ src/app/components/home/section-presentation/section-presentation.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class SectionPresentationComponent {
 
+
+  descargarCV() {
+    const link = document.createElement('a');
+    link.href = 'assets/cv/CV_ORE_LARA.pdf'; 
+    link.download = 'Harry_Ore_Lara_CV.pdf'; 
+    link.target = '_blank';
+    link.click();
+  }
+  
+
 }
